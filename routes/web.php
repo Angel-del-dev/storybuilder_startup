b9d0c87aa41c626f91ee352fb23eda0f7cae4acd
Angel <angelnovo15@gmail.com>
2025-06-11 19:33:35 +0200

    Remove junk

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'Create'])->name('home');
 
-require __DIR__.'/settings.php';
+require __DIR__.'/guest.php';
 require __DIR__.'/auth.php';
